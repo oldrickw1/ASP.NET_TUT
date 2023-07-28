13e4cbbf3b4ddd9d307dd230e9340d2b85b4e06e
Oldrick Weenink <o.weenink@hotmail.com>
2023-07-28 15:32:20 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program {
+    public static void Main() {
+        // String[] names = new String[3];
+        // names[0] = "Harry";
+        // names[1] = "Ron";
+        // names[2] = "Hermione";
+
+        String[] names = {"Harry", "Ron", "Hermione"};
+        for(int i = 0; i < names.Count(); i++) {
+            Console.WriteLine(names[i]);
+        }
+        foreach (String s in names) {
+            Console.WriteLine(s);
+        }
+    }
+
+    public static string Capitalize(string s) {
+        return s[0].ToString().ToUpper() + s.Substring(1).ToLower();
+    }
+}
